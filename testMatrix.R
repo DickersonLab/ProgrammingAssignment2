66cfee0c16ad81bbd3108213961a552d57b09a61
DickersonLab <DickersonLab@users.noreply.github.com>
2014-10-26 10:53:05 -0500

    Create testMatrix.R
    This is the test code for the cacheMatrix Exercise

---
+++ testMatrix.R
@@ -0,0 +1,32 @@
+## Based on Test code from Discussion Forum post by Gregory D Horne
+#set up a random square matrix o 0's and 1's
+# occasionally the matrix will not be invertible, if so just try again
+mdim <- 5
+A <- round(matrix(runif(mdim  * mdim ), mdim , mdim ))
+Ainv <- solve(A)
+Ainv
+A1 <- makeMatrix(A)
+A1$get()
+#should repeat A 
+A1$getinv()
+#NULL
+cacheMatrix(A1)
+#A inverse
+A1$getinv()  # this is  to show you that the matrix inv has been stored and does not affect anything
+#A inverse
+cacheMatrix(A1)
+#getting cached data
+#A inverse
+
+#Try a new matrix
+mdim <-6
+A1$set(round(matrix(runif(mdim  * mdim ), mdim , mdim )))
+A1$getinv()
+#NULL
+cacheMatrix(A1)
+#[1] 25
+cacheMatrix(A1)
+
+A1$get()
+
+
