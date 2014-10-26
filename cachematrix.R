f96b74b7bcfb997383742f02e2c15b816ee6843a
DickersonLab <DickersonLab@users.noreply.github.com>
2014-10-26 09:51:32 -0500

    Update cachematrix.R
    Added in the calling function CacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## This function either computes or retrieves the matrix inverse if it is available
 ## it currently uses the solve function to find the inverse, however ginv (moore-penrose generalized inverse) from the MASS package is more
 ## general and would be a better choice. Completee error checking for the existence of the inverse
 ## is not included as the assignment assumes that the matrix is invertible.
+##
+## Written by Julie A Dickerson, Oct 2014, adapted from code provided by course called cacheman.R
 
 makeCacheMatrix <- function(x = matrix()) {
-
-    m <- NULL
-    set <- function(y) {
-      x <<- y
-      m <<- NULL
-    }
-    get <- function() x
-    setinv <- function(solve) m <<- solve
-    getinv <- function() m
-    list(set = set, get = get,
-         setinv = setinv,
-         getinv = getinv)
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
   }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+}
+
+# cachematrix routine for calling getters and setters
+cachematrix <- function(x, ...) {
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
